76cac7f43fef0c43fa34364a87f2eba8d31ee2f7
CLEMENT <banmekeifeoluwa@gmail.com>
2026-07-09 09:18:07 +0100

    Update footer.tsx

--- components/cbt/footer.tsx
+++ components/cbt/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className="sticky bottom-0 w-full border-t border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-14 items-center justify-center px-4">
         <p className="text-sm text-muted-foreground text-center">
-          © developer — Clement IfeOluwa - ICEPAB ❄️ 🧊
+          © developer — Clement IfeOluwa - ICEPAB ❄️🧊
         </p>
       </div>
     </footer>
